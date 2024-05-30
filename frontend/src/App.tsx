07fa59e82594f0c08810ba7f7942798cd74af687
Raninder Dhillon <37101702+raninder@users.noreply.github.com>
2024-05-30 19:07:38 -0400

    updated routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {
   Route,
   Routes,
   Navigate,
+  Search,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 
@@ -10,7 +11,18 @@ const App = () => {
   return(
     <Router>
       <Routes>
-        <Route path="/" element = {<Layout/>}></Route>
+        <Route path="/" element = {<Layout>
+          <p>Home Page</p>
+          </Layout>}></Route>
+
+        <Route
+          path="/search"
+          element={
+            <Layout>
+             <p>Search Page</p>
+            </Layout>
+          }
+        />
       </Routes>
     </Router>
   )
