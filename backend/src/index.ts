6df142720fd1f312af961d19542e5c222368a410
Raninder Dhillon <37101702+raninder@users.noreply.github.com>
2024-06-05 12:57:36 -0400

    routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -10,9 +11,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
-app.get("/api/test", (req,res)=> {
-res.json({message:"hello from express"})
-})
+// //for testing express initially
+// app.get("/api/test", (req,res)=> {
+// res.json({message:"hello from express"})
+// })
+
+app.use("/api/users", userRoutes)
 
 app.listen(8000, ()=>{
     console.log("server running on local;host:8000")
